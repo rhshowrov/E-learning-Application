aa0adeb1fce39497079ac5cec8a75c59e7683beb
rakibulhasanshowrov <rakibulhasanshowrov@outlook.com>
2024-10-10 04:07:00 +0600

    "add_card design"

--- e_Learning_Application/settings.py
+++ e_Learning_Application/settings.py
@@ -137,7 +137,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_DIR = BASE_DIR / "media"
 MEDIA_URL = "/media/"
-
+MEDIA_ROOT=BASE_DIR/'media'
 # Expire the session when the user closes the browser
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 # Set session timeout to 30 minutes (1800 seconds)
--- student/views.py
+++ student/views.py
@@ -1,13 +1,37 @@
 from django.shortcuts import render
 from student.models import Student
+from teacher.models import TeacherEnrolled
 # Create your views here.
 def stdHomePage(request,student_id):
   
   student=Student.objects.get(user=request.user)
-  course=student.student_enrollments.all()
-  print(course[0].student)
+  enrolled_courses=student.student_enrollments.all()
+  course_data=[]
+  for enrollment in enrolled_courses:
+    course = enrollment.course  # Access the course instance 
+    # Get course details
+    course_name = course.name
+    course_code = course.code
+    course_section = course.section
+    course_pic = course.course_pic
+    print(course_pic)
+    # Get the teacher enrolled in this course
+    try:
+        course_instructor = TeacherEnrolled.objects.get(course=course).teacher.fullname
+    except TeacherEnrolled.DoesNotExist:
+        course_instructor = "No teacher assigned"
+    
+    # Append course and instructor details to the list
+    course_data.append({
+        'course_name': course_name,
+        'course_code': course_code,
+        'course_section': course_section,
+        'course_pic': course_pic,
+        'course_instructor': course_instructor
+    })
+    print(course_data)
   return render(request,'stdbase.html',context={
     "student_id":student_id,
     'profile':student,
-    'course':course,
+    'course':course_data,
   })
