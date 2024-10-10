b827a59a3893027ad988635b98e46ba429cb8a40
rakibulhasanshowrov <rakibulhasanshowrov@outlook.com>
2024-10-10 20:48:06 +0600

    "fixed student card style"

---
+++ student/templatetags/custom_filters.py
@@ -0,0 +1,8 @@
+from django import template
+from urllib.parse import urlparse, parse_qs
+
+register = template.Library()
+
+@register.filter(name='range_filter')
+def range_filter(value):
+  return value[:50]+'....'
