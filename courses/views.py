637ec27ebfcdc25dd436db77041f1a7f5d02ccaf
rakibulhasanshowrov <rakibulhasanshowrov@outlook.com>
2024-10-16 01:16:59 +0600

    "added class materials"

--- courses/views.py
+++ courses/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
-
+from .models import CourseFile,Course
+from django.shortcuts import get_object_or_404
 # Create your views here.
 def courseContent(request,pk):
-  
+  course=get_object_or_404(Course, pk=pk)
+  print(course)
+  course_files = CourseFile.objects.filter(course=course).order_by("date")
+  print(course_files)
+  # Check file type and add 'is_pdf' or 'is_pptx' flag
+  for file in course_files:
+      file.is_pdf = file.file.url.lower().endswith('.pdf')
+      file.is_pptx = file.file.url.lower().endswith('.pptx')
   return render(request,'course/courseDetails.html',context={
     'pk':pk,
+    'course_files':course_files,
   })
   
